548ac9e7f4188e969c2b99e6d885397f50a1c7cc
desko27 <desko27@gmail.com>
2015-03-07 11:37:31 +0100

    angular project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name asknsolveApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the asknsolveApp
+ */
+angular.module('asknsolveApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name asknsolveApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the asknsolveApp
+ */
+angular.module('asknsolveApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
