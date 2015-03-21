b7379e5b09a5c24287419758211b8c7f250e8b5c
desko27 <desko27@gmail.com>
2015-03-21 17:56:39 +0100

    loading screen and login proto

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -8,9 +8,18 @@
  * Controller of the asknsolveApp
  */
 angular.module('asknsolveApp')
-  .controller('LoginCtrl', function ($scope, $timeout) {
+  .controller('LoginCtrl', function ($scope, $http, $routeParams, $window) {
     
     // no bars
     $scope.$parent.bars = false;
+    
+    // get code from google
+    // console.log($routeParams);
+    
+    // get google auth redirection
+    $http.post('http://localhost:8000/login/', {'redirect_uri': 'http://127.0.0.1:9000/'})
+      .success(function(data) {
+        $window.location.href = data.url;
+    });
     
   });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,18 +8,22 @@
  * Controller of the asknsolveApp
  */
 angular.module('asknsolveApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $location) {
   
     // user data
     $scope.user = {
         nick: 'Desko27',
         name: 'Ismael',
         surname: 'Ramón',
-        rank: 12,
+        rank: 3,
         exp: 0.2
     };
     
     // topbar & sidebar
     $scope.bars = true;
+    
+    // finish please-wait screen
+    // ** don't finish it in login screen since it's gonna be fast-redirected
+    if ($location.$$path !== '/login') window.loading_screen.finish();
     
   });
