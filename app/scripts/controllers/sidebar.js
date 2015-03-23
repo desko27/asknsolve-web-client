a511189dd381aa16632f081d3ee1b04fe6e61efa
desko27 <desko27@gmail.com>
2015-03-23 17:38:34 +0100

    better sidebar navigation

--- app/scripts/controllers/sidebar.js
+++ app/scripts/controllers/sidebar.js
@@ -8,10 +8,18 @@
  * Controller of the asknsolveApp
  */
 angular.module('asknsolveApp')
-  .controller('SidebarCtrl', function ($scope) {
+  .controller('SidebarCtrl', function ($scope, $location) {
     
     // include javascript for sidebar directly from sb-admin-2
     $.getScript('bower_components/startbootstrap-sb-admin-2/dist/js/sb-admin-2.js');
+
+    // highlight current view menu item
+    $scope.highlightClass = function(path) {
+        if ($location.$$path === path)
+            return "current";
+        else
+            return "";
+    }
     
     // send ready event
     $scope.$emit('readySidebar');
