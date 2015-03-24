fdb7463e0ba256744fe7424c726a22bc657c0be0
desko27 <desko27@gmail.com>
2015-03-24 13:38:35 +0100

    colors for sidebar menu items

--- app/scripts/controllers/sidebar.js
+++ app/scripts/controllers/sidebar.js
@@ -12,44 +12,48 @@ angular.module('asknsolveApp')
     
     // menu items
     $scope.items = [
-        { name: 'Personal',             icon: 'user',           path: '#',
+        { name: 'Personal', icon: 'user', path: '#',
             children: [
             
-            { name: 'Dashboard',            icon: 'dashboard',      path: '/' },
-            { name: 'Nuevo ticket',         icon: 'plus',           path: '/new' },
-            { name: 'Todos mis tickets',    icon: 'ticket',         path: '/all',       tag: 'Nuevo',     tag_style: 'success' },
-            { name: 'Destacados',           icon: 'star',           path: '/starred' },
+            { name: 'Dashboard', icon: 'dashboard', path: '/' },
+            { name: 'Nuevo ticket', icon: 'plus', path: '/new' },
+            { name: 'Todos mis tickets', icon: 'ticket', path: '/all', color: 'purple', tag: 'Nuevo', tag_style: 'success' },
+            { name: 'Destacados', icon: 'star', path: '/starred', color: 'red' },
             
         ] },
-        { name: 'Equipo de soporte',    icon: 'life-ring',      path: '#',
-            children: [
-        
-            { name: 'Bandeja de entrada',   icon: 'inbox',          path: '/inbox',         news: 12 },
-            { name: 'En proceso',           icon: 'cogs',           path: '/progress',      news: 4 },
-            { name: 'Finalizado',           icon: 'check-square-o', path: '/finished' },
-            { name: 'Cuarentena',           icon: 'thumbs-o-down',  path: '/quarantine' },
-            { name: 'Estadística',          icon: 'bar-chart',      path: '/statistics' },
-            
-        ] },
-        { name: 'Miembros de Sistemas', icon: 'list-alt',       path: '#',
+        { name: 'Equipo de soporte', icon: 'life-ring', path: '#',
             children: [
         
-            { name: 'Ismael Ramón',         icon: 'male',           path: '#',      tag: 'Staff',     tag_style: 'warning' },
-            { name: 'Paqui Pardo',          icon: 'male',           path: '#',      tag: 'Staff',     tag_style: 'warning' },
-            { name: 'Daniel Durán',         icon: 'male',           path: '#',      tag: 'Staff',     tag_style: 'warning' },
+            { name: 'Bandeja de entrada', icon: 'inbox', path: '/inbox', color: 'purple', news: 12 },
+            { name: 'En proceso', icon: 'cogs', path: '/progress', color: 'purple', news: 4 },
+            { name: 'Finalizado', icon: 'check-square-o', path: '/finished', color: 'green' },
+            { name: 'Cuarentena', icon: 'thumbs-o-down', path: '/quarantine', color: 'orange' },
+            { name: 'Estadística', icon: 'bar-chart', path: '/statistics', color: 'aqua' },
             
         ] },
-        { name: 'Top Usuarios',         icon: 'trophy',         path: '/top' },
-        // { name: 'Inventario',           icon: 'database',       path: '/inventory' },
-        { name: 'Configuración',        icon: 'cog',            path: '/config' },
+        { name: 'Miembros de Sistemas', icon: 'list-alt', path: '#',
+            children: [
+        
+            { name: 'Ismael Ramón', icon: 'male', path: '#', color: 'gray', tag: 'Staff', tag_style: 'warning' },
+            { name: 'Paqui Pardo', icon: 'male', path: '#', color: 'gray', tag: 'Staff', tag_style: 'warning' },
+            { name: 'Daniel Durán', icon: 'male', path: '#', color: 'gray', tag: 'Staff', tag_style: 'warning' },
+            
+        ] },
+        // { name: 'Inventario', icon: 'database', path: '/inventory' },
+        { name: 'Top Usuarios', icon: 'trophy', path: '/top' },
+        { name: 'Configuración', icon: 'cog', path: '/config', color: 'gray' },
     ];
 
+    // default color class
+    $scope.defaultColorClass = function(color) {
+        if (color === '') return 'blue';
+        else return color;
+    };
+    
     // highlight current view menu item
     $scope.highlightClass = function(path) {
-        if ($location.$$path === path)
-            return 'current';
-        else
-            return '';
+        if ($location.$$path === path) return 'current';
+        else return '';
     };
     
     // send ready event
